f5ef69f2feb3168b5e53494f63b70ff291326b26
Konstantin Runov <runebone1@gmail.com>
2022-04-02 15:23:07 +0300

    dwm: no default blue status line

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#268db2";
-static char selbgcolor[]            = "#005577";
+static char selbgcolor[]            = "#222222";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
